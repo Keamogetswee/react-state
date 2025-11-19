5ba1ebf56d10a66254e2cb1a26392a7f29e1e83d
Kea Moseki <neoentlemoseki@gmail.com>
2025-11-19 16:28:45 +0200

    Updated Main function functionality - able to append user input to arry

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,35 @@
+import React from "react"
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    } 
+
     return (
         <main className="main">
-            <input type="text" placeholder="e.g. carrots" className="main--input" />
-            <button className="add--ingredient-button">+ Add ingredient</button>
-
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                    className="main--input"
+                />
+                <button className="add--ingredient-button">Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
